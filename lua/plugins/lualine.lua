4a0ec7726c4477cefbc872cdfa1148297fba1311
Aaron Osterhage <aaron.osterhage@garmin.com>
2022-06-10 22:14:43 -0400

    Add LSP server name to lualine
    Adds the current buffer's attached LSP server name to lualine. Also
    rearrange lualine components.
    
    Change-Id: Id63a3060509832185031175718b5b04ff9ff996e

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,16 @@
 local lualine = require('lualine')
 
+local function lsp_name()
+  local name = ''
+
+  local active_clients = vim.lsp.get_active_clients({bufnr = vim.fn.bufnr('%')})
+  if #active_clients ~= 0 then
+    name = active_clients[1].name
+  end
+
+  return name
+end
+
 lualine.setup({
 	options = {
 		disabled_filetypes = { 'alpha' },
@@ -7,15 +18,15 @@ lualine.setup({
 	},
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {
+    lualine_c = {'diff'},
+    lualine_b = {
       {
         'filename',
         path = 1,
       }
     },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}}
+    lualine_x = {'diagnostics', lsp_name, 'filetype'},
+    lualine_y = {'progress', 'location'},
+    lualine_z = {'fileformat', 'encoding'}}
 })
 
